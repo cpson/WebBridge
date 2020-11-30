2b847e99743ef853899ec993fdacc020f28ad218
cpson <cpson@mt.co.kr>
2020-11-30 09:26:58 +0900

    remove build, pods files

--- Example/Pods/Target Support Files/Pods-WebBridge_Example/Pods-WebBridge_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WebBridge_Example : NSObject
-@end
-@implementation PodsDummy_Pods_WebBridge_Example
-@end
--- Example/Pods/Target Support Files/WebBridge/WebBridge-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WebBridge : NSObject
-@end
-@implementation PodsDummy_WebBridge
-@end
