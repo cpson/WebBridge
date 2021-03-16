1541bd1521979f497aef92c900512f0e25f0f23a
손창빈 <cpson@mt.co.kr>
2021-03-16 16:07:26 +0900

    필요 없는 부분 제거

--- Sources/WebBridge/Extensions.swift
+++ Sources/WebBridge/Extensions.swift
@@ -6,15 +6,3 @@
 //
 
 import UIKit
-
-extension UIView {
-    func viewAddWithAutoLayout(_ contentView: UIView) {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.frame = contentView.frame
-        contentView.addSubview(self)
-        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: contentView, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: contentView, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: contentView, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
-        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: contentView, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
-    }
-}
